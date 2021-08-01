967999121c26f69ee71878fe47c08423ee98e2da
Trung Nguyen <57174311+trungnt2910@users.noreply.github.com>
2021-08-01 11:40:04 +0700

    feat: Initial commit

---
+++ Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UselessLib
+{
+    public class Class1
+    {
+        public static void Test()
+        {
+            System.Diagnostics.Debug.WriteLine("I am useless LMAO!");
+        }
+    }
+}
